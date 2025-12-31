8b8fa1ceb86de579ee9315d6e650b9db0c436da5
P7RASAD <92856514+P7RASAD@users.noreply.github.com>
2025-12-31 08:57:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# First number 5
+# Second number 10
+#addition
+num1 = 5
+num2 = 10
+print (num1 + num2)
+
+# Subtraction
+print (num1 - num2)
+
+#Multiplication
+print (num1 * num2)
+
+#Division
+print (num1 / num2)
