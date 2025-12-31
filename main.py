be730a288cc7d1381e98a36a7428a3f74d6003ed
P7RASAD <92856514+P7RASAD@users.noreply.github.com>
2025-12-31 22:07:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # First number 5
 # Second number 10
 #addition
+
+
 num1 = 5
 num2 = 10
 print (num1 + num2)
@@ -13,3 +15,12 @@ print (num1 * num2)
 
 #Division
 print (num1 / num2)
+
+#
+first_name = "John"
+last_name = "Doe"
+
+#Expected output (Hello, jhon Doe! Welcome to the python program.
+one = f"hello, {first_name} {last_name}! Welcome to the python program"
+
+print(one)
