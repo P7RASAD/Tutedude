25a2316270a4f8015a92fd24a53448a0634e54c0
P7RASAD <92856514+P7RASAD@users.noreply.github.com>
2026-01-09 18:29:55 +0530

    Add files via upload
    update with (input)

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 # First number 5
 # Second number 10
-#addition
-
-
+# Addition
 num1 = 5
 num2 = 10
-print (num1 + num2)
+print(num1 + num2)
 
 # Subtraction
-print (num1 - num2)
+print(num1 - num2)
 
-#Multiplication
-print (num1 * num2)
+# Multiplication
+print(num1 * num2)
 
-#Division
-print (num1 / num2)
+# Division
+print(num1 / num2)
 
-#
-first_name = input("John")
-last_name = input("Doe")
+# User input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
-#Expected output (Hello, jhon Doe! Welcome to the python program.
-one = f"hello, {first_name} {last_name}! Welcome to the python program"
 
-print(one)
-
+# Hello, John Doe! Welcome to the python program.
+message = f"Hello, {first_name} {last_name}! Welcome to the python program."
+print(message)
