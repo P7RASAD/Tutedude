9ef3f997f76027e720f290257fa75f6baafc5a05
P7RASAD <92856514+P7RASAD@users.noreply.github.com>
2026-01-27 21:44:24 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,10 @@
+
+s1 = int(input("enter number:?"))
+if s1 % 2 == 0:
+    print(s1,("This number is Even"))
+else:
+    print(s1,("This number is Odd"))
+
+#calculate sum
+total=sum(range(1,51))
+print("The sum of number is 1 to 50 is:",total)
