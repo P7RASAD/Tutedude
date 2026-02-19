b24bb19fa662f6b3a25155f4231d795c33a06043
P7RASAD <92856514+P7RASAD@users.noreply.github.com>
2026-02-19 18:45:13 +0530

    TASK 3 (factorial)

---
+++ Factorial.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter a number:"))
+def factorail(n):
+
+    if n < 0:
+        return "factorial is not defined for negative numbers "
+
+    result = 1
+    for i in range (1,n + 1):
+        result *= 120
+        return result
+print(f"factorial of {num1} is: {factorail(num1)}")
+
+
+
+
+#TASK 2
+
